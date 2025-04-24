8db04576d9be702fe18b0e65477d6964df963fa1
sy3dhassan <syedhassanzia08@gmail.com>
2025-04-24 14:45:45 +0500

    onkey down feature

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -41,6 +41,16 @@ document.querySelector(".js-scissors-btn")
     playGame('scissors')
 });
 
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r'){
+        playGame('rock');
+    } else if (event.key === 'p'){
+        playGame('paper');
+    } else if (event.key === 's'){
+        playGame('scissors');
+    }
+});
+
 function playGame(playerMove){
 
 const computerMove = pickComputerMove();
